6af9ae9775a1a534a1b9a6f95eb35e547a7bfb9e
Ines Said <104031422+Said-Ines@users.noreply.github.com>
2023-12-11 12:57:23 +0100

    Qr scanner screen (#5)
    Qr scanner screen done

--- lib/modules/design_sytem/ds_module.dart
+++ lib/modules/design_sytem/ds_module.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import 'package:spiraw/modules/design_sytem/ds_controller.dart';
+import 'package:spiraw/modules/design_sytem/ds_screen.dart';
 import 'ds_controller.dart';
 import 'ds_screen.dart';
 import 'package:spiraw/modules/design_sytem/ds2_screen.dart';
@@ -6,6 +8,6 @@ import 'package:spiraw/modules/design_sytem/ds2_screen.dart';
 
 final designSystemModule = GetPage(
   name: '/designSystemModule',
-  page: () => const DS2Screen(),
-  // binding: BindingsBuilder.put(() => DesignSystemController()),
+  page: () => const DesignSystemScreen(),
+  binding: BindingsBuilder.put(() => DesignSystemController()),
 );
